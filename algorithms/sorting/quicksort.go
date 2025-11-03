42188c1813deb417e0e7b3fd2ada7b8e8a859a5e
Jamie Garner <jamiegarner123@gmail.com>
2025-11-03 16:48:25 +1100

    randomise quicksort pivot

--- algorithms/sorting/quicksort.go
+++ algorithms/sorting/quicksort.go
@@ -1,5 +1,7 @@
 package sorting
 
+import "math/rand/v2"
+
 func QuickSort(data []int) []int {
 	if len(data) <= 1 {
 		return data
@@ -36,14 +38,20 @@ func quickSortHelper(data []int, start, end int) {
 		return
 	}
 
-	pivotIndex := end
+	// Choose a random pivot to make worst case less likely (could also choose median of 3 values)
+	pivotIndex := rand.IntN(end-start+1) + start
+
+	// Swap pivot to the end to make partitioning logic easier
+	data[pivotIndex], data[end] = data[end], data[pivotIndex]
+	pivotIndex = end
+
 	pivot := data[pivotIndex]
 	pivotBoundary := start
 
 	// Swap anything less than or equal to pivot to the pivotBoundary and increment pivotBoundary
 	// Outcome => anything less than pivot will be on left side of pivotBoundary, anything more than will be on right side
 	for i := start; i < end; i++ {
-		if data[i] <= pivot {
+		if data[i] < pivot {
 			data[i], data[pivotBoundary] = data[pivotBoundary], data[i]
 			pivotBoundary++
 		}
